c0c29767adf749bba924cc24b6e24b1b01b1a97a
Donatien D <ddlu.web@icloud.com>
2020-11-17 13:06:56 +0100

    Ability to add a new task implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,19 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+    # The line above allows to edit the variable which are locked by default (security layer)
+    # Equivalent of the attr_writer
+  end
 end
